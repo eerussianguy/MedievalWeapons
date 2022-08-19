51090977868ca1b4639984dfb5a68c0b6f8e8301
EERussianguy <eerussianguy@gmail.com>
2022-08-18 23:25:55 -0400

    lose more mixins

--- src/main/java/net/medievalweapons/item/NinjatoItem.java
+++ src/main/java/net/medievalweapons/item/NinjatoItem.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.Tier;
 public class NinjatoItem extends SwordItem
 {
 
+    // todo: i guess this is the offhand attacking thing
     public NinjatoItem(Tier toolMaterial, int attackDamage, float attackSpeed, Properties settings)
     {
         super(toolMaterial, attackDamage, attackSpeed, settings);
